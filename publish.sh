3e9ef5c15d854f7bcbc8c5711e9022439daf0c1a
Jamesbarford <jamesbarfordevans@gmail.com>
2020-07-22 08:02:26 +0100

    actually compile the files 🤦‍♂️

--- publish.sh
+++ publish.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-cp README.md ./src && cd ./src;
+if [ ! -d dist ]; then
+  mkdir dist
+fi
+tsc;
+cp ./src/package.json ./dist/package.json;
+cp ./src/package-lock.json ./dist/package-lock.json;
+cp README.md ./dist && cd ./dist;
 npm publish;
 rm README.md;
 echo "published to npm"
