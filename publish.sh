214f91105b01873726fded0fa9d6c9a10c0a03b4
Jamesbarford <jamesbarfordevans@gmail.com>
2020-07-23 21:17:57 +0100

    changed shell script and published

--- publish.sh
+++ publish.sh
@@ -8,5 +8,4 @@ cp ./src/package.json ./dist/package.json;
 cp ./src/package-lock.json ./dist/package-lock.json;
 cp README.md ./dist && cd ./dist;
 npm publish;
-rm README.md;
 echo "published to npm"
